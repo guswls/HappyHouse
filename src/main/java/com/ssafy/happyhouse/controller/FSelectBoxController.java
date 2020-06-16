7d2fe425e6fd4d49f89739a7a0582efbe4e18120
soleileun <raemong21@cau.ac.kr>
2020-06-16 19:31:39 +0900

    edit news

--- src/main/java/com/ssafy/happyhouse/controller/FSelectBoxController.java
+++ src/main/java/com/ssafy/happyhouse/controller/FSelectBoxController.java
@@ -170,52 +170,13 @@ public class FSelectBoxController extends HttpServlet {
 		return entity;	
 	}
 	
-	   @RequestMapping(value = "/news")
-	   public ResponseEntity<Map<String, Object>> todayNews() throws IOException {
-		  ResponseEntity<Map<String, Object>> entity = null;
-	     try {
-		  String url ="https://land.naver.com/news/headline.nhn?bss_ymd=20200615";
-	   
-	      
-	      Document doc =Jsoup.connect(url).get();
-	      Elements element = doc.select("div.section_headline");
-	      String title = element.select("ul").text();
-	      
-	      List<String> temp = new ArrayList<>();
-	      for(Element el : element.select("li")) {
-	         //System.out.println(el.text());
-	    	  temp.add(el.text());       
-	         System.out.println(el.text());
-	      }
-	      
-	      List<String> news = new ArrayList<>();
-	      boolean[] chk = new boolean[temp.size()];
-	      while(news.size() <5) {
-	    	  double rv = Math.random();    
-	          int iv = (int)(rv * temp.size());
-	          if(!chk[iv]) {
-	        	  news.add(temp.get(iv));
-	        	  chk[iv] = true;
-	          }
-	      }
-	      System.out.println(news);
-		
-	      entity = handleSuccess(news);
-	     }catch (RuntimeException e) {
-				entity = handleException(e);
-			}
-			System.out.println(entity + "이거는 news");
-			return entity;
-	   }
-	   
 
-	   
 	
     @RequestMapping(value = "/news")
     public ResponseEntity<Map<String, Object>> todayNews() throws IOException {
       ResponseEntity<Map<String, Object>> entity = null;
       try {
-      String url ="https://land.naver.com/news/headline.nhn?bss_ymd=2020061";
+      String url ="https://land.naver.com/news/headline.nhn?bss_ymd=20200616";
     
        
        Document doc =Jsoup.connect(url).get();
